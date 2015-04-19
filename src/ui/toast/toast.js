6e5b0e0e3d7a4efcf275a56bedb348151f022041
Tony <coolhzb@163.com>
2015-04-19 10:31:45 +0800

    add the ui/toast...

---
+++ src/ui/toast/toast.js
@@ -0,0 +1,75 @@
+/**
+ * Directive to display global error or info messages.
+ * Enqueue messages through the toastSvc.
+ */
+
+
+
+angular.module('coreos.ui')
+.directive('cfToast', function() {
+  'use strict';
+
+  return {
+    templateUrl: '/cf.ui/toast/toast.html',
+    restrict: 'E',
+    replace: true,
+    scope: true,
+    controller: function($scope, toastSvc) {
+      $scope.messages = toastSvc.messages;
+      $scope.dismiss = toastSvc.dismiss;
+    }
+  };
+});
+
+
+angular.module('cf.services')
+.factory('toastSvc', function($timeout) {
+  'use strict';
+
+  var AUTO_DISMISS_TIME = 5000,
+      service,
+      lastTimeoutPromise;
+
+  function dequeue() {
+    if (service.messages.length) {
+      service.messages.shift();
+    }
+  }
+
+  function enqueue(type, text) {
+    service.messages.push({
+      type: type,
+      text: text
+    });
+    lastTimeoutPromise = $timeout(dequeue, AUTO_DISMISS_TIME);
+  }
+
+  function cancelTimeout() {
+    if (lastTimeoutPromise) {
+      $timeout.cancel(lastTimeoutPromise);
+    }
+  }
+
+  service = {
+
+    messages: [],
+
+    error: enqueue.bind(null, 'error'),
+
+    info: enqueue.bind(null, 'info'),
+
+    dismiss: function(index) {
+      cancelTimeout();
+      service.messages.splice(index, 1);
+    },
+
+    dismissAll: function() {
+      cancelTimeout();
+      service.messages.length = 0;
+    }
+
+  };
+
+  return service;
+
+});
