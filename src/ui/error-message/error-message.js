4234e9c1713f29a92f5669489c5fe91221b3d41a
Tony <coolhzb@163.com>
2015-04-25 23:25:51 +0800

    add the error message ...

---
+++ src/ui/error-message/error-message.js
@@ -0,0 +1,63 @@
+/**
+ * Displays a message based on a promise.
+ */
+
+angular.module('cf.ui')
+
+.provider('errorMessageSvc', function() {
+  'use strict';
+
+  var formatters = {};
+
+  this.registerFormatter = function(name, fn) {
+    formatters[name] = fn;
+  };
+
+  this.$get = function() {
+    return {
+      getFormatter: function(name) {
+        return formatters[name] || angular.noop;
+      }
+    };
+  };
+
+})
+
+.directive('cfErrorMessage', function(errorMessageSvc) {
+  'use strict';
+
+  return {
+    templateUrl: '/cf.ui/error-message/error-message.html',
+    restrict: 'E',
+    replace: true,
+    scope: {
+      promise: '=',
+      formatter: '@',
+      customMessage: '@message'
+    },
+    controller: function postLink($scope) {
+      $scope.show = false;
+
+      function handler(resp) {
+        if ($scope.formatter) {
+          $scope.message =
+            errorMessageSvc.getFormatter($scope.formatter)(resp);
+        } else if ($scope.customMessage) {
+          $scope.message = $scope.customMessage;
+        } else {
+          return;
+        }
+        $scope.show = true;
+      }
+
+      $scope.$watch('promise', function(promise) {
+        $scope.show = false;
+        if (promise && promise.catch) {
+          promise.catch(handler);
+        }
+      });
+
+    }
+  };
+
+});
